99f26fdc9bf26f4acf52ede1fda2896ba23b2754
gloriaguilar <gloriaac98@gmail.com>
2023-03-06 10:11:39 -0500

    Vercel changes

--- api/index.php
+++ api/index.php
@@ -1,5 +1,3 @@
 <?php
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
-// Forward Vercel requests to normal index.php
+
 require __DIR__ . '/../public/index.php';
